6d2ff6923b87fae6263c089a3f17bc86a3e89427
Steve Swinsburg <steve.swinsburg@gmail.com>
2014-10-28 13:34:09 +0000

    [maven-release-plugin]  copy for tag sakai-wicket-maven-archetype-1.5.0

---
+++ tags/sakai-wicket-maven-archetype-1.5.0/src/main/resources/archetype-resources/impl/src/java/dao/impl/ThingMapper.java
@@ -0,0 +1,38 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.dao.impl;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import ${package}.model.Thing;
+
+/**
+ * RowMapper to handle Things
+ *
+ * @author Steve Swinsburg (steve.swinsburg@anu.edu.au)
+ *
+ */
+public class ThingMapper implements RowMapper{
+	
+	/* (non-Javadoc)
+	 * @see org.springframework.jdbc.core.RowMapper#mapRow(java.sql.ResultSet, int)
+	 */
+	@Override
+	public Thing mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		Thing t = new Thing();
+		
+		t.setId(rs.getLong("ID"));
+		t.setName(rs.getString("TITLE"));
+		
+		return t;
+	}
+	
+	
+	
+}
